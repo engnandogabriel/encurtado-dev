eb92fd59f879c880fe4147769a5692f4c5204b1b
engnandogabriel <eng.nandogabriel@gmail.com>
2024-08-01 14:33:54 -0300

    first commit

---
+++ src/main/java/com/encurtado_dev/EncurtadoDevApplication.java
@@ -0,0 +1,13 @@
+package com.encurtado_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadoDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadoDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encurtado_dev/EncurtadoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtado_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
