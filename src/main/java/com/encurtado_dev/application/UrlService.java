0eb52837b0318d5fa30c700abc78195c19de6306
engnandogabriel <eng.nandogabriel@gmail.com>
2024-08-02 15:11:45 -0300

    added handlers return

--- src/main/java/com/encurtado_dev/application/UrlService.java
+++ src/main/java/com/encurtado_dev/application/UrlService.java
@@ -1,6 +1,10 @@
 package com.encurtado_dev.application;
 
 import com.encurtado_dev.domain.DTO.ShortenUrlDTO;
+import com.encurtado_dev.domain.DTO.ShortenUrlResponseDTO;
+import com.encurtado_dev.domain.Exception.NotFoundError;
+import com.encurtado_dev.domain.HandlersService.HandlerDTO;
+import com.encurtado_dev.domain.HandlersService.Handlers;
 import com.encurtado_dev.domain.entites.Url;
 import com.encurtado_dev.domain.repository.UrlRepository;
 import com.encurtado_dev.domain.value_objects.HashCode;
@@ -15,25 +19,34 @@ public class UrlService {
         this.urlRepository = urlRepository;
     }
 
-    public void shorten(ShortenUrlDTO shortenUrlDTO,  String localUrl){
+    public HandlerDTO shorten(ShortenUrlDTO shortenUrlDTO, String localUrl){
         try {
             HashCode hash;
             do {
                 hash = new HashCode();
             } while (this.urlRepository.existByHash(hash.getHashCode()));
-            QrCode qrCode = new QrCode(localUrl+hash.getHashCode());
-            Url url = Url.create(shortenUrlDTO.longUrl(), hash.getHashCode(), qrCode.getQrCode());
+            String shortenedUrl = localUrl + hash.getHashCode();
+            QrCode qrCode = new QrCode(shortenedUrl);
+            Url url = Url.create(shortenUrlDTO.longUrl(), shortenedUrl, hash.getHashCode(), qrCode.getQrCode());
             this.urlRepository.save(url);
-        } catch (Exception e){}
+            ShortenUrlResponseDTO shortenUrlResponseDTO = new ShortenUrlResponseDTO(url.getLongUrl(), shortenedUrl, url.getHash(), url.getQrCode());
+            return new Handlers<>().success(shortenUrlResponseDTO);
+        } catch (RuntimeException e){
+            return new Handlers<>().servrError(e);
+        } catch (Exception e){
+            return new Handlers<>().badRquest(e);
+        }
 
     }
-    public String getLink(String hash){
+    public HandlerDTO getLink(String hash){
         try {
             Url url = this.urlRepository.findByHash(hash).orElse(null);
-            if(url == null) return "";
-            return url.getLongUrl();
-        } catch (Exception e){
-            return "";
+            if(url == null) return new Handlers<>().notFound(new NotFoundError("URL não encontrada"));
+            return new Handlers<>().success(url.getLongUrl());
+        } catch (RuntimeException e) {
+            return new Handlers<>().servrError(e);
+        } catch (Exception e) {
+            return new Handlers<>().badRquest(e);
         }
     }
 }
