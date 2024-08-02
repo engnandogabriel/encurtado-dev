22fc4ea902ea7c072334ba9ae03c986282a4100d
engnandogabriel <eng.nandogabriel@gmail.com>
2024-08-02 14:21:28 -0300

    added UrlRepositoryMongo

---
+++ src/main/java/com/encurtado_dev/infra/Repository/UrlRepositoryMongo.java
@@ -0,0 +1,44 @@
+package com.encurtado_dev.infra.Repository;
+
+import com.encurtado_dev.domain.repository.UrlRepository;
+import com.encurtado_dev.domain.entites.Url;
+import com.encurtado_dev.infra.Models.MongoDB;
+import com.encurtado_dev.infra.Models.UrlModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UrlRepositoryMongo implements UrlRepository {
+
+    private MongoDB mongoDB;
+    public UrlRepositoryMongo(MongoDB mongoDB) {
+        this.mongoDB = mongoDB;
+    }
+
+    @Override
+    public void save(Url url) {
+        UrlModel urlModel = new UrlModel(url.getUrlId(), url.getLongUrl(), url.getHash(), url.getQrCode(), url.getCreatedAt());
+        this.mongoDB.save(urlModel);
+    }
+
+    @Override
+    public Optional<Url> findById(String id) {
+        UrlModel urlModel = this.mongoDB.findById(id).orElse(null);
+        if(urlModel == null) return Optional.empty();
+        return Optional.of(Url.restore(urlModel.getUrlId(), urlModel.getLongUrl(), urlModel.getHash(), urlModel.getQrCode(), urlModel.getCreatedAt()));
+    }
+
+    @Override
+    public Optional<Url> findByHash(String hash) {
+        UrlModel urlModel = this.mongoDB.findByHash(hash).orElse(null);
+        if(urlModel == null) return Optional.empty();
+        return Optional.of(Url.restore(urlModel.getUrlId(), urlModel.getLongUrl(), urlModel.getHash(), urlModel.getQrCode(), urlModel.getCreatedAt()));
+    }
+
+    @Override
+    public boolean existByHash(String hash) {
+        Boolean exits = this.mongoDB.existsByHash(hash);
+        return exits;
+    }
+}
